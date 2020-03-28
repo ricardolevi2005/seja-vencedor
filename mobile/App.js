f3ab39c394b50c12b4f02b5495ddec7f63aac54a
Ricardo Levi <ricardolevi2005@hotmail.com>
2020-03-28 14:36:44 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
+import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+import Routes from './src/routes.js'
+
+
+
+export default function App() {
+  return (
+    <Routes />
+  )}
+
